981ad8d000c8c4ca6132a8f2ab89c4b86faf8788
Simon Jenni <simujenni@gmail.com>
2017-09-05 10:37:57 +0200

    train model on imagenet

--- train_imagenet.py
+++ train_imagenet.py
@@ -1,12 +1,12 @@
 from Preprocessor import Preprocessor
-from train.SDNetTrainer import SDNetTrainer
+from train.SDNetTrainer_linear_decay import SDNetTrainer
 from datasets.ImageNet import ImageNet
-from models.SDNet_avgDisc_concat_new_2 import SDNet
+from models.SDNet_avgDisc_concat_new_3 import SDNet
 
-target_shape = [128, 128, 3]
-model = SDNet(num_layers=5, batch_size=128, target_shape=target_shape)
+target_shape = [160, 160, 3]
+model = SDNet(num_layers=5, batch_size=64, target_shape=target_shape, pool5=True, disc_pad='SAME', tag='new_default')
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=target_shape, augment_color=True)
-trainer = SDNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=180, tag='baseline',
-                       lr_policy='linear', optimizer='adam', init_lr=0.0003, end_lr=1e-8)
+trainer = SDNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=180, tag='new_default',
+                       lr_policy='linear', optimizer='adam', init_lr=0.0003, end_lr=0.000003)
 trainer.train()
